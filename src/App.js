35131992a7871dd71f82bb12f6fd8952fb233201
Lubinets Sergey Nikolaevich <snlubinets@skr.local>
2023-05-22 09:48:54 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  const [text, setText] = useState(`Div ${count}`);
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleDivClick = (event) => {
+    event.target.contentEditable = true;
+  };
+
+  const showDivs = () => {
+    const divs = [];
+    for (let i = 0; i < count; i++) {
+      divs.push(<div 
+                  key={i} 
+                  draggable={true} 
+                  onClick={handleDivClick} 
+                  onInput={handleTextChange} 
+                  style={{width:'50px', height:'50px', border:'1px solid black'}}
+                  >
+                    {text}
+                  </div>);
+    }
+    return divs;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleClick}>Add Div</button>
+      {showDivs()}
     </div>
   );
 }
